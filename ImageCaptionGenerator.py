4d46a2869484ceb02092e5c0b605f3561c2f363a
varshasrinivas95 <varug95@gmail.com>
2023-05-11 11:19:20 -0700

    changes

--- ImageCaptionGenerator.py
+++ ImageCaptionGenerator.py
@@ -61,7 +61,8 @@ import zipfile
 # 
 #
 #
-#--------------------------------------------------
+#-----------------------------------------------------------       
+
 def load_captions(zip_file_path,file_to_access):
     # Extract the file from the zip file
     with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
@@ -70,14 +71,15 @@ def load_captions(zip_file_path,file_to_access):
             return(content)
 #-----------------------------------------------------------       
 
-#--------------------------------------------------
+#-----------------------------------------------------------       
+
 # Each photo has a unique identifier, which is the file name of the image .jpg file
 # Create a dictionary of photo identifiers (without the .jpg) to captions. Each photo identifier maps to
 # a list of one or more textual descriptions.
 #
 # {"image_name_1" : ["caption 1", "caption 2", "caption 3"],
 #  "image_name_2" : ["caption 4", "caption 5"]}
-#--------------------------------------------------
+#-----------------------------------------------------------       
 def captions_dict (text):
   dict = {}
   ## Converting Bytes to string
@@ -105,102 +107,37 @@ def captions_dict (text):
       dict [image_name].append (caption)
   return (dict)
 
-print("Loading Text File")
+print("Retrieving text files from zip folder")
 doc = load_captions ("download_ds_file.zip","Flickr8k.token.txt")
 image_dict = captions_dict (doc)
-print(image_dict)
+#-----------------------------------------------------------       
 
-# Importing Libraries
-# import tensorflow as tf
-# import numpy as np
-# import os
-# import time
-# import json
-# from PIL import Image
-# import pickle
+#-----------------------------------------------------------       
+# We have three separate files which contain the names for the subset of 
+# images to be used for training, validation or testing respectively
+#
+# Given a file, we return a set of image names (without .jpg extension) in that file
+#--------------------------------------------------
+def subset_image_name (train_img_txt):
+  data = []
+  ## Converting Bytes to string
+  train_img_txt = train_img_txt.decode('utf-8')
+  # Make a List of each line in the file
+  lines = train_img_txt.split ('\n')
+  for line in lines:
+        # skip empty lines
+        if (len(line) < 1):
+              continue
+        # Each line is the <image_file>
+        # Split the <image_file> into <image_name>.jpg
+        image_name = line.split ('.')[0]
+        # Add the <image_name> to the list
+        data.append (image_name)
 
-## Data Preprocessing
-# class Preprocessor:
-#     def __init__(self, img_folder_path, annotation_file_path, max_vocab_size, max_length):
-#         self.img_folder_path = img_folder_path
-#         self.annotation_file_path = annotation_file_path
-#         self.max_vocab_size = max_vocab_size
-#         self.max_length = max_length
+  return (set(data))  
 
-#     def get_image_names(self):
-#         """
-#         This function returns a list of all the image file names in the folder.
-#         """
-#         return os.listdir(self.img_folder_path)
-
-#     def load_image(self, img_path):
-#         """
-#         This function loads the image from the given path and resizes it to the required size.
-#         """
-#         img = tf.io.read_file(self.img_folder_path + '/' + img_path)
-#         img = tf.image.decode_jpeg(img, channels=3)
-#         img = tf.image.resize(img, (299, 299))
-#         img = tf.keras.applications.inception_v3.preprocess_input(img)
-#         return img, img_path
-
-#     def load_annotations(self):
-#         """
-#         This function reads the annotations file and returns the image name and the corresponding caption.
-#         """
-#         with open(self.annotation_file_path, 'r') as f:
-#             annotations = json.load(f)
-
-#         # Store captions and image names in separate lists
-#         all_captions = []
-#         all_img_names = []
-
-#         for annot in annotations['annotations']:
-#             caption = '<startseq> ' + annot['caption'] + ' <endseq>'
-#             img_name = annot['image_id']
-#             all_img_names.append(img_name)
-#             all_captions.append(caption)
-
-#         return all_img_names, all_captions
-
-#     def tokenize_captions(self, captions_list):
-#         """
-#         This function tokenizes the captions and returns the tokenizer along with the tokenized captions.
-#         """
-#         # Initialize the tokenizer
-#         tokenizer = tf.keras.preprocessing.text.Tokenizer(num_words=self.max_vocab_size, oov_token="<unk>", filters='!"#$%&()*+.,-/:;=?@[\]^_`{|}~ ')
-        
-#         # Fit the tokenizer on the captions
-#         tokenizer.fit_on_texts(captions_list)
-        
-#         # Tokenize the captions
-#         tokenized_captions = tokenizer.texts_to_sequences(captions_list)
-        
-#         # Pad the tokenized sequences
-#         captions_vector = tf.keras.preprocessing.sequence.pad_sequences(tokenized_captions, padding='post', maxlen=self.max_length)
-        
-#         return tokenizer, captions_vector
-
-#     def preprocessed_data(self):
-#         """
-#         This function returns the preprocessed images and their corresponding captions.
-#         """
-#         # Get image names
-#         all_img_names = self.get_image_names()
-        
-#         # Get image features
-#         image_features_extract_model = tf.keras.applications.InceptionV3(include_top=False, weights='imagenet')
-#         new_input = image_features_extract_model.input
-#         hidden_layer = image_features_extract_model.layers[-1].output
-#         image_features_extract_model = tf.keras.Model(new_input, hidden_layer)
-#         img_features = {}
-
-#         for count, img_name in enumerate(all_img_names):
-#             img, path = self.load_image(img_name)
-#             batch_features = image_features_extract_model(img)
-#             batch_features = tf.reshape(batch_features, (batch_features.shape[0], -1, batch_features.shape[3]))
-#             img_features[path] = batch_features.numpy()
-
-#             if count % 1000 == 0:
-#                 print(f"Processed {count} images")
-
-        # Save image
+print("Retrieving names of training images from text file")
+training_imgname_doc = load_captions("download_ds_file.zip","Flickr_8k.trainImages.txt")
+training_image_names = subset_image_name (training_imgname_doc)
+print("Image name and captions of training images")
+#-----------------------------------------------------------       
