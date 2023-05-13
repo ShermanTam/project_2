503c470ec78700c6995d1b709928a4dfe7073dd3
varshasrinivas95 <varug95@gmail.com>
2023-05-13 13:59:42 -0700

    cg

--- ImageCaptionGenerator.py
+++ ImageCaptionGenerator.py
@@ -175,34 +175,34 @@ import tensorflow as tf
 from tqdm import tqdm
 import numpy as np
 
-def load_image(image_path):
-    img = tf.io.read_file(image_path)
-    img = tf.image.decode_jpeg(img, channels=3)
-    img = tf.image.resize(img, (299, 299))
-    img = tf.keras.applications.inception_v3.preprocess_input(img)
-    return img, image_path
+# def load_image(image_path):
+#     img = tf.io.read_file(image_path)
+#     img = tf.image.decode_jpeg(img, channels=3)
+#     img = tf.image.resize(img, (299, 299))
+#     img = tf.keras.applications.inception_v3.preprocess_input(img)
+#     return img, image_path
 
-def process_image_dataset(image_dir, training_image_names):
-    image_model = tf.keras.applications.InceptionV3(include_top=False, weights='imagenet')
-    new_input = image_model.input
-    hidden_layer = image_model.layers[-1].output
-    image_features_extract_model = tf.keras.Model(new_input, hidden_layer)
+# def process_image_dataset(image_dir, training_image_names):
+#     image_model = tf.keras.applications.InceptionV3(include_top=False, weights='imagenet')
+#     new_input = image_model.input
+#     hidden_layer = image_model.layers[-1].output
+#     image_features_extract_model = tf.keras.Model(new_input, hidden_layer)
 
-    training_image_paths = [image_dir + name + '.jpg' for name in training_image_names]
-    encode_train = sorted(set(training_image_paths))
+#     training_image_paths = [image_dir + name + '.jpg' for name in training_image_names]
+#     encode_train = sorted(set(training_image_paths))
 
-    image_dataset = tf.data.Dataset.from_tensor_slices(encode_train)
-    image_dataset = image_dataset.map(load_image, num_parallel_calls=tf.data.experimental.AUTOTUNE).batch(16)
+#     image_dataset = tf.data.Dataset.from_tensor_slices(encode_train)
+#     image_dataset = image_dataset.map(load_image, num_parallel_calls=tf.data.experimental.AUTOTUNE).batch(16)
 
-    for img, path in tqdm(image_dataset):
-        batch_features = image_features_extract_model(img)
-        batch_features = tf.reshape(batch_features, (batch_features.shape[0], -1, batch_features.shape[3]))
+#     for img, path in tqdm(image_dataset):
+#         batch_features = image_features_extract_model(img)
+#         batch_features = tf.reshape(batch_features, (batch_features.shape[0], -1, batch_features.shape[3]))
 
-        for bf, p in zip(batch_features, path):
-            path_of_feature = p.numpy().decode("utf-8")
-            np.save(path_of_feature, bf.numpy())
+#         for bf, p in zip(batch_features, path):
+#             path_of_feature = p.numpy().decode("utf-8")
+#             np.save(path_of_feature, bf.numpy())
 
-# Example usage:
-image_dir = "/content/drive/MyDrive/Flickr8/Flicker8k_Dataset/"  # Add your list of training image names
+# # Example usage:
+# image_dir = "/content/drive/MyDrive/Flickr8/Flicker8k_Dataset/"  # Add your list of training image names
 
-process_image_dataset(image_dir, training_image_names)
+# # process_image_dataset(image_dir, training_image_names)
