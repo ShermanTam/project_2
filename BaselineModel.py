44f26b85d1ff7ec9f8f9d7b01c98de97842c2518
varshasrinivas95 <varug95@gmail.com>
2023-05-19 19:41:41 -0700

    updated baseline

--- BaselineModel.py
+++ BaselineModel.py
@@ -14,6 +14,9 @@ from nltk.translate.bleu_score import corpus_bleu
 import zipfile
 from sklearn.model_selection import train_test_split
 
+print("Epoch number:", os.environ.get('EPOCH_NUMBER'))
+print("Batch number:", os.environ.get('BATCH_SIZE'))
+print("Model Type:",  os.environ.get('MODEL_TYPE'))
 
 class ImageCaptionGenerator:
     def __init__(self):
@@ -64,6 +67,141 @@ class ImageCaptionGenerator:
         with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
             with zip_ref.open(file_to_access) as file:
                 content = file.read().decode('utf-8')
+    
+                # return(content)
+
+        mapping = {}
+        for line in tqdm(content.split('\n')):
+            tokens = line.split(',')
+            if len(line) < 2:
+                continue
+            image_id, caption = tokens[0], tokens[1:]
+            image_id = image_id.split('.')[0]
+            caption = " ".join(caption)
+            if image_id not in mapping:
+                mapping[image_id] = []
+            mapping[image_id].append(caption)
+
+        self.mapping = mapping
+
+    def clean_captions(self):
+        for key, captions in self.mapping.items():
+            for i in range(len(captions)):
+                caption = captions[i]
+                caption = caption.lower()
+                # Remove punctuation marks
+                caption = "".join([c if c.isalpha() else " " for c in caption])
+                # Remove extra whitespaces
+                caption = " ".join(caption.split())
+                # Update the cleaned caption
+                captions[i] = caption
+
+    def create_tokenizer(self):
+        all_captions = []
+        for key, captions in self.mapping.items():
+            all_captions.extend(captions)
+
+        tokenizer = Tokenizer()
+        tokenizer.fit_on_texts(all_captions)
+        self.tokenizer = tokenizer
+        self.vocab_size = len(tokenizer.word_index) + 1
+
+    def create_sequences(self):
+        sequences = []
+        for key, captions in self.mapping.items():
+            for caption in captions:
+                # Convert caption to sequence of integers
+                sequence = self.tokenizer.texts_to_sequences([caption])[0]
+                # Generate multiple input-output pairs for the sequence
+                for i in range(1, len(sequence)):
+                    in_seq, out_seq = sequence[:i], sequence[i]
+                    in_seq = pad_sequences([in_seq], maxlen=self.max_length)[0]
+                    out_seq = to_categorical([out_seq], num_classes=self.vocab_size)[0]
+                    sequences.append((key, in_seq, out_seq))
+
+        return sequences
+
+    def generate_data(self, sequences):
+        X_image, X_sequence, y = [], [], []
+        for key, in_seq, out_seq in sequences:
+            # Retrieve image features
+            image_feature = self.features[key][0]
+            # Append the data to respective lists
+            X_image.append(image_feature)
+            X_sequence.append(in_seq)
+            y.append(out_seq)
+
+        return np.array(X_image), np.array(X_sequence), np.array(y)
+
+    def define_model(self):
+        # Image feature input
+        inputs1 = Input(shape=(4096,))
+        x1 = Dropout(0.5)(inputs1)
+        x2 = Dense(256, activation='relu')(x1)
+
+        # Sequence input
+        inputs2 = Input(shape=(self.max_length,))
+        y1 = Embedding(self.vocab_size, 256, mask_zero=True)(inputs2)
+        y2 = Dropout(0.5)(y1)
+        y3 = LSTM(256)(y2)
+
+        # Decoder model
+        decoder1 = add([x2, y3])
+        decoder2 = Dense(256, activation='relu')(decoder1)
+        outputs = Dense(self.vocab_size, activation='softmax')(decoder2)
+
+        # Merge the models
+        model = Model(inputs=[inputs1, inputs2], outputs=outputs)
+        model.compile(loss='categorical_crossentropy', optimizer='adam')
+
+        self.model = model
+
+    def generate_caption(self, photo):
+        # Preprocess the image
+        img = load_img(photo, target_size=(224, 224))
+        img = img_to_array(img)
+        img = img.reshape((1, img.shape[0], img.shape[1], img.shape[2]))
+        img = preprocess_input(img)
+
+        # Extract features from the image
+        feature = self.model.predict(img, verbose=0)
+
+        # Generate a sequence of integers
+        sequence = [self.tokenizer.word_index['startseq']]
+
+        # Generate the caption word by word
+        for _ in range(self.max_length):
+            # Pad the sequence
+            sequence = pad_sequences([sequence], maxlen=self.max_length)
+
+            # Predict the next word
+            yhat = self.model.predict([feature, sequence], verbose=0)
+
+            # Convert probability to integer index
+            yhat = np.argmax(yhat)
+
+            # Map integer index to word
+            word = self.tokenizer.index_word[yhat]
+
+            # Stop if we predict the end of the sequence
+            if word == 'endseq':
+                break
+
+            # Append the predicted word to the sequence
+            sequence[0].append(yhat)
+
+        # Remove start and end tokens from the sequence
+        caption = sequence[0][1:-1]
+
+        # Convert the sequence of integers to a caption
+        caption = [self.tokenizer.index_word[word] for word in caption]
+        caption = ' '.join(caption)
+
+        return caption
+
+# Instantiate the ImageCaptionGenerator
+generator = ImageCaptionGenerator()
+
 print("Loading VGG16 model")
 generator.extract_image_features()
 
@@ -73,7 +211,7 @@ generator.load_image_features("datasets/Flicker8k_Dataset")
 print("Loading captions data from the dataset file")
 generator.load_captions_data("datasets/download_ds_file.zip","Flickr8k.token.txt")
 
-print("Preprocessing the captions:")
+print("Preprocessing the captions")
 generator.clean_captions()
 
 print("\t tokenization and other preprocessing") 
@@ -98,15 +236,9 @@ X_image_train, X_image_val, X_sequence_train, X_sequence_val, y_train, y_val = t
 # Define the model
 generator.define_model()
 
-
 # Train the model using the generated data
-print("Training the model:")
-
-
-print("\t Epoch number:", os.environ.get('EPOCH_NUMBER'))
-print("\t Batch number:", os.environ.get('BATCH_SIZE'))
-
-generator.model.fit([X_image_train, X_sequence_train], y_train, batch_size=os.environ.get('BATCH_SIZE'), epochs=os.environ.get('EPOCH_NUMBER'), verbose=1,
+print("Training the model")
+generator.model.fit([X_image_train, X_sequence_train], y_train, batch_size=32, epochs=10, verbose=1,
                     validation_data=([X_image_val, X_sequence_val], y_val))
 generator.model.fit([X_image_train, X_sequence_train], y_train, 
                     validation_data=([X_image_val, X_sequence_val], y_val),
