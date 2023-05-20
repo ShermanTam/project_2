f2349d45a713e3a6f3ead8fa7590506f532f1b87
varshasrinivas95 <varug95@gmail.com>
2023-05-20 00:01:10 -0700

    c

--- BaselineModel.py
+++ BaselineModel.py
@@ -157,43 +157,8 @@ class ImageCaptionGenerator:
             if idx == index:
                 return word
         return None
-    # def evaluate_model(self, test_image_path):
-    #     # Load the test image
-    #     img = cv2.imread("datasets/Flicker8k_Dataset/1001773457_577c3a7d70.jpg")
-    #     if img is None:
-    #         print("loading 1001773457_577c3a7d70.jpg for evaluation purpose")
-    #         return
-
-    #     # Preprocess the test image
-    #     target_size = (224, 224)
-    #     img = cv2.resize(img, target_size)
-    #     image = img_to_array(img)
-    #     image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
-    #     image = preprocess_input(image)
-
-    #     # Generate image features using the pre-trained model
-    #     test_image_feature = self.model.predict(image, verbose=0)
-
-    #     # Generate a caption for the test image
-    #     start_token = self.tokenizer.word_index['startseq']
-    #     end_token = self.tokenizer.word_index['endseq']
-    #     caption = 'startseq'
-    #     for _ in range(self.max_length):
-    #         sequence = self.tokenizer.texts_to_sequences([caption])[0]
-    #         sequence = pad_sequences([sequence], maxlen=self.max_length)
-    #         yhat = self.model.predict([test_image_feature, sequence], verbose=0)
-    #         yhat = np.argmax(yhat)
-    #         word = self.get_word_from_index(yhat)
-    #         if word is None:
-    #             break
-    #         caption += ' ' + word
-    #         if yhat == end_token:
-    #             break
-
-    #     # Print the generated caption
-    #     print("Generated Caption:", caption)
-
-   def evaluate_model(self, test_image_path):
+    
+    def evaluate_model(self, test_image_path):
         # Load the test image
         img = cv2.imread("datasets/Flicker8k_Dataset/1001773457_577c3a7d70.jpg")
         if img is None:
